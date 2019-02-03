69af74527ca8b024a28f5b1538c36dbc9ccc1e8d
Dana L. Brown <dana.l.brown@gmail.com>
2019-02-03 11:52:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 
      
+<script src="https://cdn.jsdelivr.net/npm/vue@2.5.22/dist/vue.js"></script>  
+<script>
+	
+    var vm = new Vue({
+	el:  "#jobs",
+	data: {
+		jobs: [
+			{ name: "Hardware Engineer", category: "Tech" },
+			{ name: "Budget Analyst", category: "Accounting" },
+			{ name: "Content Strategist", category: "Marketing" },
+			{ name: "Business Development Manager", category: "Sales" },
+			{ name: "Network Administrator", category: "Tech" },
+			{ name: "Risk Analyst", category: "Accounting" },
+			{ name: "Social Media Specialist", category: "Marketing" },
+			{ name: "Territory Account Manager", category: "Sales" },
+			{ name: "Scrum Master", category: "Tech" }
+		],
+		selectedCategory: "All"
+	},
+	computed: {
+		filteredjobs: function() {
+			var vm = this;
+			var category = vm.selectedCategory;
+			
+			if(category === "All") {
+				return vm.jobs;
+			} else {
+				return vm.jobs.filter(function(job) {
+					return job.category === category;
+				});
+			}
+		}
+	}
+});
+</script>
