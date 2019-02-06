0dc6378efc11bf004e0b2586a38a7e1235f8bd38
Dana L. Brown <dana.l.brown@gmail.com>
2019-02-06 11:41:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
 	
-    var vm = new Vue({
-	el:  "#jobs",
-	     // declare message with an empty value
-           message: ''
-           },
-        template: '<div>{{ message }}</div>'})
-	data: {
-       
-		jobs: [
-			{ name: "Hardware Engineer", category: "Tech" },
-			{ name: "Budget Analyst", category: "Accounting" },
-			{ name: "Content Strategist", category: "Marketing" },
-			{ name: "Business Development Manager", category: "Sales" },
-			{ name: "Network Administrator", category: "Tech" },
-			{ name: "Risk Analyst", category: "Accounting" },
-			{ name: "Social Media Specialist", category: "Marketing" },
-			{ name: "Territory Account Manager", category: "Sales" },
-			{ name: "Scrum Master", category: "Tech" }
-		],
-		selectedCategory: "All"
-	},
-
-    	computed: {
-		filteredJobs: function() {
-			var vm = this;
-			var category = vm.selectedCategory;
-			
-			if(category === "All") {
-				return vm.jobs;
-			} else {
-				return vm.jobs.filter(function(job) {
-					return job.category === category;
-				});
-			}
-		}
-	}
-});
-     </script>
-   </body>
-</html>
+   
+var app = new Vue({
+	el: '#app',
+  
+  data() {
+    return {
+      apiRequests:[],
+      search: '',
+      filterText: '',
+    }
+  },
+  
+  // Filter for Displaying blogs
+  computed: {
+    filteredBlogs() {
+    	console.log(this.search, this.filterText, this.apiRequests);
+      return this.apiRequests.filter((apiRequest) => {
+      	let regexp = new RegExp(`(?=.*${this.filterText})(?=.*${this.search})`, 'g');
+				return apiRequest.title.match(regexp);
+      })
+    }
+  },
+  
+  // Getting data from API
+  created() {
+    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data) {
+      this.apiRequests = data.body.slice(0,5);
+      console.log(data);
+    })
+  },
+  
+  methods: {
+  	filter(event) {
+    	this.filterText = event.target.checked ? event.target.value : '';
+    }
+  }
+  
+})
