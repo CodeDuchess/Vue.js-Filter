45404ef30b57a38f919dfd1e3ee60899b1471b20
Dana L. Brown <dana.l.brown@gmail.com>
2019-03-31 19:34:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,68 @@
-	
-    var vm = new Vue({
-	el:  "#jobs",
-	     // declare message with an empty value
-           message: ''
-           },
-        template: '<div>{{ message }}</div>'})
-	data: {
-       
-		jobs: [
-			{ name: "Hardware Engineer", category: "Tech" },
-			{ name: "Budget Analyst", category: "Accounting" },
-			{ name: "Content Strategist", category: "Marketing" },
-			{ name: "Business Development Manager", category: "Sales" },
-			{ name: "Network Administrator", category: "Tech" },
-			{ name: "Risk Analyst", category: "Accounting" },
-			{ name: "Social Media Specialist", category: "Marketing" },
-			{ name: "Territory Account Manager", category: "Sales" },
-			{ name: "Scrum Master", category: "Tech" }
-		],
-		selectedCategory: "All"
-	},
+'use strict';
 
-    	computed: {
+Vue.component('job-card', {
+    props: ['job'],
+    template: '<div class="card"><h1>{{ job.title }}</h1><p>{{ this.jobDescription }}</p><p><small>{{ job.type }}</small></p></div>',
+    computed: {
+        jobDescription: function() {
+            
+            return this.job.description.substring(0, 300) + '...';
+        }
+    }
+  });
+
+  var vm = new Vue({
+	el:  "#jobs",
+	data() { 
+	  return {
+          apiData:[],
+          search: '',
+          filterType: [],
+          
+          selectedType:"Full Time"
+        }
+    },
+      
+    computed: {
 		filteredJobs: function() {
-			var vm = this;
-			var category = vm.selectedCategory;
+			vm = this;
+			var type = vm.selectedType;
 			
-			if(category === "All") {
-				return vm.jobs;
+			if(type == "Full Time") {
+				return vm.apiData;
 			} else {
-				return vm.jobs.filter(function(job) {
-					return job.category === category;
+				return vm.apiData.filter(function(job) {
+					return job.type == type;
 				});
-			}
+            }
 		}
-	}
+    },
+
+    methods: { 
+        getJobData: function() {
+            let url = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?&markdown=true&page=1';
+            fetch( url,{
+                'headers': {
+                  'Content-Type': 'application/json'
+                },
+              })
+              .then((response) => {
+                return response.json();
+              
+              })
+              .then((rJson) => {
+                  this.apiData = rJson;
+              })
+        }
+    },
+
+    mounted: function() {
+        this.$nextTick(function() {
+            this.getJobData();
+        });
+    }
+
+
 });
-     </script>
-   </body>
-</html>
+  
+
